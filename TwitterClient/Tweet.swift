555e079ff588511484b6886d53e8a6d06b4e9922
chenrangong <chenran.gong@sv.cmu.edu>
2016-10-30 01:04:21 -0700

    first commit

---
+++ TwitterClient/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  TwitterClient
+//
+//  Created by Chenran Gong on 10/29/16.
+//  Copyright © 2016 Chenran Gong. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
